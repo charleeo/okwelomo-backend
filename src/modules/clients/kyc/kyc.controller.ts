956ec683b0eaf7d78fed81c3dbed75ed6d976c66
charleeo <charleeotaru@gmail.com>
2023-09-28 18:39:17 +0100

    Same

--- src/modules/clients/kyc/kyc.controller.ts
+++ src/modules/clients/kyc/kyc.controller.ts
@@ -5,6 +5,9 @@ import {
   Request as NestRequest,
   UseGuards,
   Res,
+  Get,
+  Query,
+  Param,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { CreateKYCDTO } from './dto/create.dto';
@@ -13,11 +16,11 @@ import { KYC } from './entities/kyc.entity';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('kyc')
+@UseGuards(AuthGuard('jwt'))
 export class KycController {
   constructor(private kcyService: KycService) {}
 
   @Post('create')
-  @UseGuards(AuthGuard('jwt'))
   async create(
     @Body() kyc: CreateKYCDTO,
     @NestRequest() req,
@@ -25,5 +28,16 @@ export class KycController {
   ): Promise<any> {
     const user = req.user;
     return await this.kcyService.create(kyc, user, res);
+  }
+
+  @Get('all')
+  async index(@Res() res: Response, @Query() query: any): Promise<any> {
+    console.log('here');
+    return await this.kcyService.index(res, query);
+  }
+
+  @Get(':id')
+  async show(@Res() res: Response, @Param() param: any): Promise<any> {
+    return await this.kcyService.show(res, param);
   }
 }
--- src/modules/clients/kyc/kyc.service.ts
+++ src/modules/clients/kyc/kyc.service.ts
@@ -1,16 +1,13 @@
-import {
-  BadRequestException,
-  HttpStatus,
-  Injectable,
-  Res,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Res, Query, Param } from '@nestjs/common';
+import { IsNumber } from 'class-validator';
 import { KYCRepository } from './repositories/kyc.repository';
 import { CreateKYCDTO } from './dto/create.dto';
 import { KYC } from './entities/kyc.entity';
 import { Users } from 'src/modules/user/entities/user.entity';
 import { logErrors } from 'src/common/helpers/logging';
 import { responseStructure } from 'src/common/helpers/response.structure';
-import { Response } from 'express';
+import { Response, query } from 'express';
+import { Pagination, paginate } from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class KycService {
@@ -27,6 +24,7 @@ export class KycService {
     let responseData = null;
 
     try {
+      //checking for an existing kyc records
       if (await this.findOne(user.id, 'user_id')) {
         message = 'KYC record exists';
         return response
@@ -36,12 +34,19 @@ export class KycService {
       kyc['user'] = user.id; //always use the key that is in your entity defination, in this case, it is user and not userId
       kyc['user_id'] = user.id;
       responseData = await this.kycRepo.save(kyc);
-      if (responseData) status = true;
+      if (responseData !== null) {
+        status = true;
+        message = 'KYC record created';
+      }
     } catch (e) {
       logErrors(e);
       error = e.message;
     }
-    return { status, error, message, response: responseData };
+    return response
+      .status(HttpStatus.CREATED)
+      .send(
+        responseStructure(status, message, responseData, HttpStatus.CREATED),
+      );
   }
 
   async findOne(value: any, fieldName: any): Promise<KYC> {
@@ -49,5 +54,70 @@ export class KycService {
       where: { [`${fieldName}`]: value },
     });
     return data;
+  }
+
+  async index(@Res() response: Response, @Query() query: any): Promise<any> {
+    let status = false;
+    let error = null;
+    let message = '';
+    let responseData = null;
+
+    try {
+      const qb = await this.kycRepo
+        .createQueryBuilder('kyc')
+        .leftJoinAndSelect('kyc.user', 'user');
+      qb.orderBy('kyc.id', 'DESC');
+      const page = query.page && IsNumber(query.page) ? query.page : 1;
+      const limit = query.limit && IsNumber(query.limit) ? query.limit : 20;
+
+      responseData = await paginate<KYC>(qb, {
+        page,
+        limit,
+        route: process.env.APP_URL + 'kyc/all',
+      });
+
+      if (responseData['items'].length > 0) {
+        message = 'Data found';
+        status = true;
+      }
+    } catch (e) {
+      logErrors(e);
+      error = e.message;
+    }
+
+    return response
+      .status(HttpStatus.CREATED)
+      .send(
+        responseStructure(status, message, responseData, HttpStatus.CREATED),
+      );
+  }
+
+  async show(@Res() response: Response, @Param() params): Promise<any> {
+    let status = false;
+    let message = '';
+    let responseData = null;
+
+    try {
+      const kycId = params.id;
+      responseData = await this.kycRepo.findOne({
+        where: { id: kycId },
+        relations: {
+          user: true,
+        },
+      });
+
+      if (responseData) {
+        message = 'KYC data found';
+        status = true;
+      } else message = 'No kyc found';
+    } catch (e) {
+      logErrors(e.message);
+    }
+
+    return response
+      .status(HttpStatus.CREATED)
+      .send(
+        responseStructure(status, message, responseData, HttpStatus.CREATED),
+      );
   }
 }
