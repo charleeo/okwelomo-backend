2c621f91e853ca03b203dc1cc1e3bf49640b4bce
charleeo <charleeotaru@gmail.com>
2023-11-14 12:20:57 +0100

    same

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -8,7 +8,7 @@ import { Users } from '../user/entities/user.entity';
 import { ValidateField } from './kyc/Validations/ValidateField';
 import { KYCInputVaidtion } from 'src/middleware/kyc/kyc.input.validation.middleware';
 import { ValidateKYCId } from './kyc/Validations/ValidateKYCId';
-import { VerifyKycMiddleware } from 'src/middleware/kyc/verify-kyc/verify-kyc.middleware';
+
 import { LoanRepository } from './loans/repositories/loan.repository';
 import { LoanSettingRepository } from './loans/repositories/loan.setting.repository';
 import { LoanSettingService } from './loans/services/loan.settings.service';
@@ -59,6 +59,9 @@ export class ClientsModule {
       );
     consumer
       .apply(IsAdminMiddleware)
-      .forRoutes({ path: '/kyc/update/status', method: RequestMethod.POST });
+      .forRoutes(
+        { path: '/kyc/update/status', method: RequestMethod.POST },
+        { path: '/loans/approve', method: RequestMethod.POST },
+      );
   }
 }
