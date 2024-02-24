41e1319170dda00f30cff2c7a8b83fd8c3a7cd81
charleeo <charleeotaru@gmail.com>
2024-02-24 13:44:33 +0100

    ok

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  # access_key = var.AWS_ACCESS_KEY_ID
+  # secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 # Create VPC
